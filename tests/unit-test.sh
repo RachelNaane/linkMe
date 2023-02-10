7a3fff3cb66f68003f14741c793d85b6926984f2
RachelNaane <ratash3@gmail.com>
2023-02-10 13:37:35 +0200

    unit test modified

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 url=${1:-http://localhost:3000}
-echo $url
+
+echo "UNIT TEST==============="
+echo "app at $url"
+echo "starting test..."
 
 response=$(curl -s -o /dev/null -w "%{http_code}" $url/health)
-echo $response
+echo "got response $response"
 
 if [ $response -eq 200 ]; then
+  echo "succes, app healthy"
   exit 0
 else
+  echo "test failed" 
   exit 1
 fi
