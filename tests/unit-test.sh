3d5bbea52699d01863bb7137f7707438c0c3eb17
RachelNaane <ratash3@gmail.com>
2023-02-10 12:54:08 +0200

    unit test modified

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 url=${1:-http://localhost:3000/health}
+echo $url
+
 response=$(curl -s -o /dev/null -w "%{http_code}" $url)
+echo $response
 
 if [ $response -eq 200 ]; then
   exit 0
